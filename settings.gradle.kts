fa450422ca8738379ee9663307cff79307ed36ba
quentin <quentinkimppienne@gmail.com>
2025-02-26 11:25:04 +0100

    Mise à jour architecture

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
 rootProject.name = "Sudoku"
-
+include("src:main:model")
+findProject(":src:main:model")?.name = "model"
+include("test")
+include("src:main:controller")
+findProject(":src:main:controller")?.name = "controller"
+include("src:main:view")
+findProject(":src:main:view")?.name = "view"
