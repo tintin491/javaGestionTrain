913ffdcc3051e8e43528c0a617e3e6b69b3aa59f
quentin <quentinkimppienne@gmail.com>
2025-03-04 10:11:48 +0100

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,2 @@
-rootProject.name = "Sudoku"
-include("src:main:model")
-findProject(":src:main:model")?.name = "model"
-include("test")
-include("src:main:controller")
-findProject(":src:main:controller")?.name = "controller"
-include("src:main:view")
-findProject(":src:main:view")?.name = "view"
+rootProject.name = "ProjetGareJava"
+
